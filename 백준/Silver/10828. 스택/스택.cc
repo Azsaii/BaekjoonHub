a94099cf54561ddf4700539438de4719a3e7e580
KIM IN WOO <120641012+Azsaii@users.noreply.github.com>
2023-02-23 23:00:26 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 1132 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 10000
+
+typedef int element;
+typedef struct {
+	element data[MAX];
+	int top;
+}stack;
+
+void init_stack(stack* s) {
+	s->top = -1;
+}
+int is_empty(stack* s) {
+	return (s->top == -1);
+}
+int is_full(stack* s) {
+	return (s->top == MAX - 1);
+}
+
+void push(stack* s, element item) {
+	if (is_full(s))
+		return;
+	else s->data[++(s->top)] = item;
+}
+element pop(stack* s) {
+	if (is_empty(s))
+		return -1;
+	else return s->data[(s->top)--];
+}
+
+element peek(stack* s) {
+	if (is_empty(s))
+		return -1;
+	else return s->data[s->top];
+}
+
+int main() {
+	stack s;
+	init_stack(&s); //초기화
+	
+	int size = 0;
+	char order[100010];
+	int n = 0;
+
+	scanf("%d", &size);
+	fgetc(stdin);
+
+	for (int i = 0; i < size; i++) {	
+		scanf("%s", order);
+		fgetc(stdin);
+
+		if (strcmp(order, "pop") == 0) {
+			printf("%d\n", pop(&s));
+		}
+
+		else if (strcmp(order, "size") == 0) {
+			printf("%d\n", s.top + 1);
+		}
+
+		else if (strcmp(order, "empty") == 0) {
+			if (is_empty(&s)) printf("1\n");
+			else printf("0\n");
+		}
+
+		else if (strcmp(order, "top") == 0) {
+			printf("%d\n", peek(&s));
+		}
+
+		else if (strcmp(order, "push") == 0) {
+			//char* num = strtok(order, " ");
+			//num = strtok(NULL, " ");
+			scanf("%d", &n);
+			//n = atoi(num);
+			push(&s, n);
+		}
+	}
+}
