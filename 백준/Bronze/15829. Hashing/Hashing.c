ddfc713d74ccd3919d4b4cc441c212e529e50f99
KIM IN WOO <120641012+Azsaii@users.noreply.github.com>
2023-02-27 22:07:32 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -1,20 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 	int size = 0;
-	char str[50];
+	char str[50] = {'0', };
 
 	scanf("%d", &size);
 	scanf("%s", str);
 
+	int rtmp = 1;
 	int r = 31;
 	int M = 1234567891;
-	int res = 0;
+	unsigned long long int res = 0;
 
-	for (int i = 0; i < size; i++) {
-		res += (str[i] - 96) * pow(r, i);
+	for (int i = 0; i < size; i++) {		
+		if(i != 0) rtmp *= r;
+		rtmp %= M;
+		res += (str[i] - 96) * rtmp;
+		res %= M;
 	}
-	printf("%d", res % M);
+	printf("%d", res);
 }
