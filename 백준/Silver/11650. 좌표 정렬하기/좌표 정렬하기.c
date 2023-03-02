32dfc380b5c90e0e39b597741910c81833cada4f
KIM IN WOO <120641012+Azsaii@users.noreply.github.com>
2023-03-02 13:18:38 +0900

    Update 좌표 정렬하기.c

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>    // qsort 함수가 선언된 헤더 파일
 #define MAX 100000
 
-typedef struct _QUESTION {
+typedef struct Point {
     int x;
     int y;
 } Point;
 
-int compare(const void* a, const void* b)    // 오름차순 비교 함수 구현
+int compare(const void* a, const void* b)    // 퀵정렬에 사용
 {
-    Point num1 = *(Point*)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
-    Point num2 = *(Point*)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+    Point num1 = *(Point*)a;    // void 포인터를 Point 포인터로 변환한 뒤 역참조하여 값을 가져옴
+    Point num2 = *(Point*)b;    
 
     if (num1.x < num2.x)    // a가 b보다 작을 때는
         return -1;      // -1 반환
@@ -40,7 +40,6 @@ int main()
         scanf("%d %d", &p[i].x, &p[i].y);
     }
 
-    // 정렬할 배열, 요소 개수, 요소 크기, 비교 함수를 넣어줌
     qsort(p, size, sizeof(Point), compare);
 
     for (int i = 0; i < size; i++)
