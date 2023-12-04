a0b1ae9864364fe10aab3175f01bd83a530daab4
KIM IN WOO <socczx@naver.com>
2023-12-04 13:57:40 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.cc
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(void){
-    int n, m, t1, t2, t3;
+    int n, m;
     cin >> n >> m;
 
     int six = 1000;
     int one = 1000;
 
     for(int i = 0; i < m; i++){
+        int t1, t2;
         cin >> t1 >> t2;
         six = t1 < six ? t1 : six;
         one = t2 < one ? t2 : one;
@@ -17,5 +19,5 @@ int main(void){
     int r1 = n * one;
     int r2 = (n/6) * six + (n%6) * one;
     int r3 = (n/6 + 1) * six;
-    cout << (r1 < r2 ? r1 < r3 ? r1 : r3 : r2 < r3 ? r2 : r3);
+    cout << min({r1, r2, r3});
 }
