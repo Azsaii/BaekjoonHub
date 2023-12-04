165e868e5933fe1c7e29e6936f58c19831726a38
KIM IN WOO <socczx@naver.com>
2023-12-04 13:27:35 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.cc
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -2,33 +2,25 @@
 using namespace std;
 
 int main(void){
-    int n, m, temp1, temp2, temp3;
-    cin >> n;
-    cin >> m;
+    int n, m, t1, t2, t3;
+    cin >> n >> m;
 
     int minPackage = 1000;
     int minPiece = 1000;
-    int p1, p2 = 0;
+    int p1 = 100000;
 
     for(int i = 0; i < m; i++){
-        cin >> temp1;
-        cin >> temp2;
-        if(temp1 < minPackage) minPackage = temp1;
-        if(temp2 < minPiece) minPiece = temp2;
+        cin >> t1 >> t2;
+        if(t1 < minPackage) minPackage = t1;
+        if(t2 < minPiece) minPiece = t2;
     }
-    // 패키지로만 사는 경우
-    int countPackage;
-    if(n % 6 == 0) countPackage = n / 6;
-    else countPackage = n / 6 + 1;
-    p1 = countPackage * minPackage;
-
-    // 패키지 + 낱개 or 낱개만으로 사는 경우
-    p2 = p1;
+    
     int r = n / 6; // 최대로 필요한 패키지 수
+    int p2 = (r+1)*minPackage;
     for(int i = 0; i <= r; i++){
-        temp3 = i * minPackage + (n - i*6) * minPiece;
-        if(temp3 < p2) p2 = temp3;
+        t3 = i * minPackage + (n - i*6) * minPiece;
+        if(t3 < p1) p1 = t3;
     }
 
-    cout << (p1 <= p2 ? p1 : p2);
+    cout << (p1 < p2 ? p1 : p2);
 }
