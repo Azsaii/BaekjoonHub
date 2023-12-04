f5ebb8e9a773181303d662c28b94cd1ee8e948aa
KIM IN WOO <socczx@naver.com>
2023-12-04 13:51:31 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.cc
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -5,22 +5,17 @@ int main(void){
     int n, m, t1, t2, t3;
     cin >> n >> m;
 
-    int minPackage = 1000;
-    int minPiece = 1000;
-    int p1 = 100000;
+    int six = 1000;
+    int one = 1000;
 
     for(int i = 0; i < m; i++){
         cin >> t1 >> t2;
-        if(t1 < minPackage) minPackage = t1;
-        if(t2 < minPiece) minPiece = t2;
-    }
-    
-    int r = n / 6; // 최대로 필요한 패키지 수
-    int p2 = (r+1)*minPackage;
-    for(int i = 0; i <= r; i++){
-        t3 = i * minPackage + (n - i*6) * minPiece;
-        if(t3 < p1) p1 = t3;
+        six = t1 < six ? t1 : six;
+        one = t2 < one ? t2 : one;
     }
 
-    cout << (p1 < p2 ? p1 : p2);
+    int r1 = n * one;
+    int r2 = (n/6) * six + (n%6) * one;
+    int r3 = (n/6 + 1) * six;
+    cout << (r1 < r2 ? r1 < r3 ? r1 : r3 : r2 < r3 ? r2 : r3);
 }
