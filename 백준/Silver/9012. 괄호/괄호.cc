bf2d3003b2e6cada369cd300a1d3698950b34f6f
KIM IN WOO <socczx@naver.com>
2023-12-17 14:52:12 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 0;
+    string str;
+    cin >> n;
+    vector<string> v(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        bool state = true;
+        int right = 0;
+        if (str.at(0) != '(' || str.at(str.size() - 1) != ')') {
+            cout << "NO" << "\n";
+            continue;
+        }
+        for (int k = 0; k < str.size(); k++) {
+            if (str.at(k) == '(') right++;
+            else if (str.at(k) == ')') {
+                if (right == 0) {
+                    cout << "NO" << "\n";
+                    state = false;
+                    break;
+                }
+                right--;
+            }
+        }
+
+        if (right != 0 && state) {
+            cout << "NO" << "\n";
+            state = false;
+            continue;
+        }
+        else if(right == 0 && state) {
+            cout << "YES" << "\n";
+        }
+    }
+}
