12775d3df13fbce371bccb8dfe23230b1d60388f
KIM IN WOO <socczx@naver.com>
2023-12-20 15:54:51 +0900

    [Gold V] Title: 리모컨, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.cc
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -34,21 +34,22 @@ int main()
 
     int gap = abs(n - 100); // |목표채널 - 현재채널|
 
-    //if (n == cur) cout << 0; // 현재채널 = 목적 채널인 경우
-    //else if (brokenCnt == 10) cout << abs(n - 100); // 모든 버튼이 고장난 경우
-    //else if (brokenCnt == 0) cout << nLength; // 고장난 버튼 없는 경우
-    //else if (nLength >= gap) cout << gap; // '+' or '-'만 사용
-    //else if (nLength < gap && brokenCnt == 0) { cout << nLength;} // 숫자만 입력
+    if (n == cur) cout << 0; // 현재채널 = 목적 채널인 경우
+    else if (brokenCnt == 10) cout << gap; // 모든 버튼이 고장난 경우
+    else if (nLength >= gap) cout << gap; // '+' or '-'만 사용
+    else if (nLength < gap && brokenCnt == 0) { cout << nLength;} // 숫자만 입력
 
-    for (int i = 0; i <= 1000000; i++)
-    {
-        if (btn_set(i) == true)
+    else { // 혼합
+        for (int i = 0; i <= 1000000; i++)
         {
-            int second_cnt = abs(n - i) + to_string(i).length();
-            gap = min(gap, second_cnt);
+            if (btn_set(i) == true)
+            {
+                int second_cnt = abs(n - i) + to_string(i).length();
+                gap = min(gap, second_cnt);
+            }
         }
+        cout << gap;
     }
-    cout << gap;
 
     return 0;
 }
