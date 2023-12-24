573a51a86736b6d318e42fefbc77a2b3871ed2d7
KIM IN WOO <socczx@naver.com>
2023-12-24 20:39:02 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -4,10 +4,13 @@
 #include <climits>
 using namespace std;
 
-int inf = INT_MAX;
-vector<int> g(400, 0);
-vector<int> fishPos;
-int res = 0, fishCnt = 0, level = 2, levelCnt = 2, n;
+struct Pos {
+    int x, y;
+};
+
+int inf = 987654321;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
 void init() {
     cin.tie(0);
@@ -15,92 +18,63 @@ void init() {
     ios_base::sync_with_stdio(false);
 }
 
-int bfs(int startIndex, int desIndex) { // 상어가 정해진 물고기를 먹으러 가는데 걸리는 최단거리 구하기
-    queue<pair<int, int>> q; // 위치와 이동거리
-    vector<bool> visited(400, false);
-    q.push(make_pair(startIndex, 0));
-    visited[startIndex] = true;
-    int distance = inf;
-    int findIndex[4] = { 1, -1, n, -n };
-
-    while (!q.empty()) {
-        int node = q.front().first;
-        int cnt = q.front().second;
-        q.pop();
-        if (node == desIndex) {
-            distance = min(distance, cnt);
-            continue;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int nextNode = node + findIndex[i];
-            // 범위가 잘못되었거나 방문되었거나 상어보다 큰 물고기인 경우 탐색불가
-            if (nextNode < 0 || nextNode >= n * n || ((i == 0 || i == 1) && node / n != nextNode / n)) continue;
-            if (visited[nextNode] || g[nextNode] > level) continue;
-            q.push(make_pair(nextNode, cnt + 1));
-            visited[nextNode] = true;
-        }
-    }
-
-    return distance;
-}
-
-int findMinFish(int startIndex) { // 먹을 수 있는 물고기 중 최단거리 물고기 구해서 반환
-    int minDistance = inf, minFish = startIndex;
-    for (int i = 0; i < fishCnt; i++) { // 물고기 수만큼 반복
-        if (g[fishPos[i]] >= level || g[fishPos[i]] == 0 || g[fishPos[i]] == 9) continue;
-        
-        int temp = bfs(startIndex, fishPos[i]); // 후보 물고기를 먹는 데 걸리는 최단거리 구하기
-        if (minDistance > temp) { // 최단거리 물고기 업데이트
-            minFish = fishPos[i];
-            minDistance = temp;  
-        }
-    }
-    if(minDistance != inf) res += minDistance;
-
-    return minFish;
-}
-
-void eat(int startIndex) { // 물고기 수만큼 최단거리 물고기 차례로 구해서 먹기
-    int cnt = fishCnt;
-    while (cnt != 0) {
-        int temp = findMinFish(startIndex);
-        if (temp == startIndex) {
-            break;
-        }
-        else {
-            g[startIndex] = 0;
-            startIndex = temp;
-            g[temp] = 9;
-
-            cnt--;
-            levelCnt--;
-            if (levelCnt == 0) {
-                level++;
-                levelCnt = level;
-            }
-        }
-    }
-}
-
 int main()
 {
     init();
-    int temp, startIndex = 0;
+    Pos sp;
+    int N, map[20][20], result = 0, size = 2, sizeCnt = 2;
     
-    cin >> n;
+    cin >> N;
 
-    for (int i = 0; i < n * n; i++) {
-        cin >> temp;
-        g[i] = temp;
-        if (temp == 9) startIndex = i;
-        else if (temp != 0) {
-            fishCnt++;
-            fishPos.push_back(i);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> map[i][j];
+            if (map[i][j] == 9) {
+                sp = { i, j };
+                map[i][j] = 0;
+            }
         }
     }
 
-    eat(startIndex);
-    cout << res;
+    while (1) {
+        bool visited[20][20] = { 0, };
+        queue<Pos> q;
+        q.push(sp);
+        visited[sp.x][sp.y] = true;
+        int dis = 0, minX, minY;
+        minX = minY = inf;
+
+        while (!q.empty()) {
+            int qsize = q.size();
+            dis++;
+            while (qsize--) {
+                Pos node = q.front();
+                q.pop();
+      
+                for (int i = 0; i < 4; i++) {
+                    int nx = node.x + dx[i];
+                    int ny = node.y + dy[i];
+                    if (nx < 0 || ny < 0 || nx == N || ny == N || map[nx][ny] > size || visited[nx][ny]) continue;
+                    if (map[nx][ny] != 0 && map[nx][ny] < size) {
+                        if (nx < minX) minX = nx, minY = ny; // 후보 물고기 갱신
+                        else if (nx == minX && ny < minY) minY = ny;
+                    }
+                    q.push({ nx, ny });
+                    visited[nx][ny] = true;
+                }
+            }
+            if (minX != inf) {
+                result += dis;
+                sp.x = minX, sp.y = minY; // 시작 지점 갱신
+                map[sp.x][sp.y] = 0;
+                sizeCnt--; 
+                if (sizeCnt == 0) size++, sizeCnt = size; // 상어 사이즈 증가
+                break;
+            }
+        }
+        if (minX == inf) break;
+    }
+
+    cout << result;
     return 0;
 }
