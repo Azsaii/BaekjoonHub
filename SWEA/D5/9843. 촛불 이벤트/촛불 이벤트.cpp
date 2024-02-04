2b3d295d91ebc13d4272d87023d8aeed3ae92357
KIM IN WOO <socczx@naver.com>
2024-02-05 01:09:01 +0900

    [D5] Title: 촛불 이벤트, Time: 104 ms, Memory: 13,536 KB -BaekjoonHub

---
+++ SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void init() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}
+int main() {
+    init();
+    int T;
+    cin >> T;
+    for (int tc = 1; tc <= T; ++tc) {
+        uint64_t N;
+        cin >> N;
+        int64_t t = sqrt(N * 2);
+        if ((t * (t + 1)) / 2 != N) t = -1;
+        cout << "#" << tc << " " << t << "\n";
+    }
+    return 0;
+}
