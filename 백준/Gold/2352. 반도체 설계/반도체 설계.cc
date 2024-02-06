6eac4f403648df354e437f026e22bcc5e46382cd
KIM IN WOO <socczx@naver.com>
2024-02-06 18:26:56 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2332 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+int v[40000], lis[40000];
+using namespace std;
+void init() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}
+int main() {
+    init();
+    int N, cnt = 0;
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> v[i];
+    lis[0] = v[0];
+    for (int i = 1; i < N; i++) {
+        if (lis[cnt] < v[i]) lis[++cnt] = v[i];
+        else {
+            int s = 0, e = cnt;
+            while (s < e) {
+                int m = (s + e) / 2;
+                if (lis[m] >= v[i]) e = m;
+                else s = m + 1;
+            }
+            lis[e] = v[i];
+        }
+    }
+    cout << ++cnt << '\n';
+    return 0;
+}
