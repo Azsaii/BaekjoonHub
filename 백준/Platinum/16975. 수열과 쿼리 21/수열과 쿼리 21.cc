7794d06a011b6594f8eed9be7f5f69a3cf6ec301
KIM IN WOO <socczx@naver.com>
2024-02-10 00:12:38 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 48 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.cc
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define MAX 100001
+using namespace std;
+typedef long long ll;
+ll ft[MAX];
+int n, m, i, j, arr[MAX];
+void init() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+}
+ll sum(int i) {
+    ll ans = 0;
+    while (i > 0) {
+        ans += ft[i];
+        i -= (i & -i);
+    }
+    return ans;
+}
+void upd(int i, int num) {
+    while (i <= n) {
+        ft[i] += num;
+        i += i & -i;
+    }
+}
+// Fenwick Tree Lazy propagation
+int main() {
+    init();
+    cin >> n;
+    arr[0] = 0;
+    for (i = 1; i <= n; i++) {
+        cin >> arr[i];
+        upd(i, arr[i] - arr[i - 1]);
+    }
+    cin >> m;
+    for (i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a;
+        if (a == 1) {
+            cin >> a >> b >> c;
+            upd(a, c);
+            upd(b + 1, -c);
+        }
+        else if (a == 2) {
+            cin >> b;
+            cout << sum(b) << '\n';
+        }
+    }
+}
