825259238a92a82a6619c5fe9dd283197b2333b4
KIM IN WOO <socczx@naver.com>
2024-02-14 16:45:28 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 49 ms, Memory: 13,584 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.cpp
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.cpp
@@ -3,6 +3,8 @@
 #include<queue>
 #define pii pair<int, int>
 using namespace std;
+int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
+int arr[100][100];
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int t, n;
@@ -11,42 +13,31 @@ int main() {
     for (int tc = 1; tc <= t; ++tc) {
         cin >> n;
         int sz = n * n;
-        vector<int> arr(sz), dist(sz, INT32_MAX);
-        vector<bool> v(sz, 0);
+        int dist[100][100] = { 0, };
+        bool v[100][100] = { 0, };
         queue<pii> q;
-        int left[4] = { -1, 1, -n, n };
-        int right[4] = { n, sz, n, n };
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 cin >> temp;
-                arr[i * n + j] = temp - '0';
+                arr[i][j] = temp - '0';
             }
         }
         q.push({ 0, 0 });
         while (!q.empty()) {
             pii cur = q.front();
             q.pop();
-            int f = cur.first, s = cur.second;
-            if (v[f] && dist[f] > arr[f] + s)continue;
-            v[f] = 1;
-            if ((f + 1) % n != 0 && dist[f + 1] > arr[f + 1] + s) { // 우
-                dist[f + 1] = arr[f + 1] + s;
-                q.push({ f + 1, arr[f + 1] + s });
-            }   
-            if ((f + n) < sz && dist[f + n] > arr[f + n] + s) { // 하
-                dist[f + n] = arr[f + n] + s;
-                q.push({ f + n, arr[f + n] + s });
-            }    
-            if (f % n != 0 && dist[f - 1] > arr[f - 1] + s) { // 좌
-                dist[f - 1] = arr[f - 1] + s;
-                q.push({ f - 1, arr[f - 1] + s });
-            }   
-            if (f >= n && dist[f - n] > arr[f - n] + s) { // 상
-                dist[f - n] = arr[f - n] + s;
-                q.push({ f - n, arr[f - n] + s });
+            int cy = cur.first, cx = cur.second;
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i], ny = cy + dy[i];
+                if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                if (!v[ny][nx] || dist[ny][nx] > dist[cy][cx] + arr[ny][nx]) {
+                    dist[ny][nx] = dist[cy][cx] + arr[ny][nx];
+                    q.push({ ny, nx });
+                    v[ny][nx] = 1;
+                }
             }
         }
-        cout << "#" << tc << ' ' << dist[sz - 1] << '\n';
+        cout << "#" << tc << ' ' << dist[n - 1][n - 1] << '\n';
     }
     return 0;
 }
