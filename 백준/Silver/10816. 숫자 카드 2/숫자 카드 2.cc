cf7bb99c9e84e4b4a6dfdc5cb32ce133cb5a2940
KIM IN WOO <socczx@naver.com>
2024-03-13 23:15:58 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 512 ms, Memory: 47836 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    int N, M, t;
+    cin >> N;
+    unordered_map<int, int> m;
+    for (int i = 0; i < N; i++) { cin >> t; if (m.find(t) == m.end())m[t] = 1; else m[t]++; }
+    cin >> M;
+    vector<int> n(M), res(M);
+    for (int i = 0; i < M; i++) { cin >> n[i]; res[i] = m[n[i]]; }
+    for (int i = 0; i < M; i++) cout << res[i] << ' ';
+    return 0;
+}
