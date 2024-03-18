ea27430abedee5539c1074ff0e66341e2b850c6c
KIM IN WOO <socczx@naver.com>
2024-03-18 22:50:26 +0900

    [Silver II] Title: 좌표 압축, Time: 796 ms, Memory: 39124 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
+#define pii pair<int, int>
 using namespace std;
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-    int n;
+    int n, t;
     cin >> n;
-    vector<int> v(n), sv;
-    map<int, bool> m;
-    for (int i = 0; i < n; i++) { cin >> v[i]; m[v[i]] = 1; }
-    for (auto it : m) sv.push_back(it.first);
-    int sz = sv.size();
+    vector<pii> v(n);
+    vector<int> r(n);
+    unordered_map<int, int> cnt;
+    for (int i = 0; i < n; i++) { cin >> t; v[i] = { t, i }; cnt[t]++; }
+    sort(v.begin(), v.end());
+    int index = 0;
     for (int i = 0; i < n; i++) {
-        int s = 0, e = sz - 1, target = v[i];
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (sv[mid] == target) { cout << mid << ' '; break; }
-            else if (sv[mid] < target) { s = mid + 1; }
-            else if (sv[mid] > target) { e = mid - 1; }
-        }
+        auto [f, s] = v[i];
+        r[s] = index;
+        if (cnt[f] == 1) index++;
+        else cnt[f]--;
     }
+    for (int i = 0; i < n; i++) cout << r[i] << ' ';
     return 0;
 }
