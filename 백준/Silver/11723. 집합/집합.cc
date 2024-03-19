4b6edfd94b129c0294e42f0c33a6bb4993c03d2a
KIM IN WOO <socczx@naver.com>
2024-03-19 22:15:19 +0900

    [Silver V] Title: 집합, Time: 492 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -2,20 +2,20 @@
 using namespace std;
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-    int M, a;
+    int M, a = 0, b;
     cin >> M;
-    vector<bool> v(21, 0);
     string s;
     for (int i = 0; i < M; i++) {
         cin >> s;
-        if (s[1] != 'l' && s[1] != 'm') cin >> a;
+        if (s[1] != 'l' && s[1] != 'm') cin >> b;
+        b--;
         switch (s[1]) {
-        case 'd': v[a] = 1; break;
-        case 'e': v[a] = 0; break;
-        case 'h': cout << v[a] << '\n'; break;
-        case 'o': v[a] ? v[a] = 0 : v[a] = 1; break;
-        case 'l': v.assign(21, 1); break;
-        case 'm': v.assign(21, 0); break;
+        case 'd': a = a | (1 << b); break;
+        case 'e': a = a & ~(1 << b); break;
+        case 'h': (a & (1 << b)) ? cout << "1\n" : cout << "0\n"; break;
+        case 'o': a = a ^ (1 << b); break;
+        case 'l': a = (1 << 20) - 1; break;
+        case 'm': a = 0; break;
         }
     }
     return 0;
