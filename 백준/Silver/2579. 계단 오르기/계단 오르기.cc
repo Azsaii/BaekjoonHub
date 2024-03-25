32a5206d73ca17c2d1d2b5d925b4012324c733e0
KIM IN WOO <socczx@naver.com>
2024-03-25 23:18:42 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -4,20 +4,12 @@ int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     int N;
     cin >> N;
-    vector<int> v(N);
-    vector<vector<int>> res(N, vector<int>(2));
+    vector<int> v(300, 0);
+    vector<vector<int>> res(300, vector<int>(2));
     for (int i = 0; i < N; i++) cin >> v[i];
-    if (N < 3) {
-        int sum = 0;
-        for (int i = 0; i < N; i++) sum += v[i];
-        cout << sum; return 0;
-    }
-    res[0][0] = v[0];
-    res[1][0] = v[1];
-    res[1][1] = v[1] + v[0];
-    res[2][0] = v[2] + v[0];
-    res[2][1] = v[2] + v[1];
-    for (int i = 3; i < N; i++) {
+    res[0][0] = res[0][1] = v[0];
+    res[1][0] = v[1]; res[1][1] = v[1] + v[0];
+    for (int i = 2; i < N; i++) {
         res[i][0] = v[i] + (res[i - 2][0] > res[i - 2][1] ? res[i - 2][0] : res[i - 2][1]);
         res[i][1] = v[i] + res[i - 1][0];
     }
