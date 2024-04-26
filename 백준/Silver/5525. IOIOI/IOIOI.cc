8e21ab3813ac173317398aee5423b596abdc1bff
KIM IN WOO <socczx@naver.com>
2024-04-26 16:48:42 +0900

    [Silver I] Title: IOIOI, Time: 24 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -3,17 +3,20 @@ using namespace std;
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     int N, M;
-    string s, str;
-    cin >> N >> M >> s;
-    int tarlen = 2 * N + 1, res = 0;
-    for (int i = 0; i < tarlen; i++) {
-        if (i % 2 == 0) str.append("I");
-        else str.append("O");
-    }
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'I') {
-            if (s.substr(i, tarlen) == str) res++;  
-        }
+    char c;
+    cin >> N >> M;
+    int tarlen = 2 * N + 1, res = 0, cnt = 0;
+    bool check = 0;
+    for (int i = 0; i < M; i++) {
+        cin >> c;
+        //cout << "c: " << c << ", cnt: " << cnt << ", res: " << res << '\n';       
+        if (check == 1 && c == 'O') { cnt = N * 2; check = 0; continue; }
+        check = 0;
+        if (cnt % 2 == 0 && c == 'I') cnt++;
+        else if (cnt % 2 == 1 && c == 'O') cnt++;
+        else if (cnt % 2 == 1 && c == 'I') cnt = 1;
+        else cnt = 0;
+        if (cnt == tarlen) { res++; cnt = 0; check = 1; }
     }
     cout << res << '\n';
 }
