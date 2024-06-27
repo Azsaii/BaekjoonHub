8cbc84ed51c90f2b4de93a9d6109023a2b702bfc
KIM IN WOO <socczx@naver.com>
2024-06-27 18:25:58 +0900

    [Silver II] Title: 과일 탕후루, Time: 56 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    int N, left = 0, right = 1, maxv = 1;
+    cin >> N;
+    unordered_map<int, int> m;
+    vector<int> v(N);
+    for(int i = 0; i < N; i++) cin >> v[i];
+    m[v[left]] = 1;
+    while(1){
+        if(right == N) break;
+        if(v[left] == v[right]) m[v[left]]++;
+        else m[v[right]]++;
+        while(m.size() == 3){
+            m[v[left]]--;
+            if(m[v[left]] == 0) m.erase(v[left]);
+            left++;
+        }
+        right++;
+        //cout << "l: " << left << ", r: " << right << '\n';
+        maxv = max(maxv, right - left);
+    }
+    cout << maxv;
+}
