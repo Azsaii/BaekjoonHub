3ce5f4850adfd209b4a1de12ed1402d41bb32a8a
KIM IN WOO <socczx@naver.com>
2024-07-05 10:53:50 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int n, m;
-vector<int> v;
-void dfs(int cur){ 
-    if(v.size() == m){
-        for(int i : v) cout << i << ' ';
-        cout << '\n';
+char v[20];
+void dfs(int ci, int cur){
+    if(ci >= m){
+        cout << v << '\n';
         return;
     }
     for(int i = cur; i <= n; i++) {
-        v.push_back(i);
-        dfs(i);
-        v.pop_back();
+        v[ci] = i + '0';
+        v[ci + 1] = ' ';
+        dfs(ci + 2, i);
     }
 }
 int main(){
     ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
     cin >> n >> m;
-    dfs(1);
+    m *= 2;
+    dfs(0, 1);
 }
