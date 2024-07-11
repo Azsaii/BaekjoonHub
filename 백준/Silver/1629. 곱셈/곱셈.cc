db4fad88e25960634fdeda64c3e84288fcfedf04
KIM IN WOO <socczx@naver.com>
2024-07-11 22:22:54 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,18 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-long long a, b, c, k;
-long long power(long long b) {
-	if (b == 0) return 1;
-	if (b == 1) return a % c;
-	
-	k = power(b / 2) % c;
-	if (b % 2 == 0) return k * k % c;
-	return k * k % c * a % c;
+ll a, b, c;
+ll f(ll x) {
+    if (x == 0) return 1;
+    if (x == 1) return a % c;
+    ll t = f(x / 2) % c;
+    if (x % 2 == 0) return t * t % c;
+    return t * t % c * a % c;
 }
-
-int main(void) {
-	cin >> a >> b >> c; 
-	cout << power(b);
-
-	return 0;
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    cin >> a >> b >> c;
+    // if(x % 2 == 0) -> a ^ x = a ^ (x / 2) * a ^ (x / 2)
+    // else -> a ^ x = a ^ (x / 2) * a ^ (x / 2 + 1) * a
+    cout << f(b);
 }
