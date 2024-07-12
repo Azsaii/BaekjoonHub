71874202e5e9c47a78afdf14c4252139353364df
KIM IN WOO <socczx@naver.com>
2024-07-12 14:06:08 +0900

    [Silver I] Title: 정수 삼각형, Time: 4 ms, Memory: 2016 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,23 +1,38 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-    int n, t;
-    cin >> n;
-    int total = n * (n + 1) / 2;
+char rbuf[1<<17];
+int idx, ridx;
+inline char read() {
+    if (idx == ridx) {
+        ridx = fread(rbuf, 1, 1<<17, stdin);
+        if (!ridx) return 0;
+        idx = 0;
+    }
+    return rbuf[idx++];
+}
+inline int readInt() {
+    int sum = 0;
+    char now = read();
+    
+    while (now <= 32) now = read();
+    while (now >= 48) sum = sum * 10 + now - '0', now = read();
+    
+    return sum;
+}
+int main() {
+    int n = readInt();
     vector<vector<int>> v(n + 1);
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j <= i; j++) {
-            cin >> t;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            int t = readInt();
             v[i].push_back(t);
         }
     }
-    for(int i = n - 1; i > 0; i--) {
-        for(int j = 0; j < v[i].size() - 1; j++) {
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < v[i].size() - 1; j++)
             v[i - 1][j] += max(v[i][j], v[i][j + 1]);
-        }
     }
-    cout << v[0][0];
+    printf("%d", v[0][0]);
+    return 0;
 }
