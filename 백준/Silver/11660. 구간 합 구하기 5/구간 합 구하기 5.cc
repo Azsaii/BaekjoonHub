e390f7f285fabbcbe18adfc0dbb059291bbbd349
KIM IN WOO <socczx@naver.com>
2024-07-13 22:33:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 124 ms, Memory: 6264 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -4,24 +4,19 @@ int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> v(n, vector<int>(n));
-    for (int i = 0; i < n; i++) {
-        int total = 0, t;
-        for (int j = 0; j < n; j++) {
+    vector<vector<int>> v(n + 1, vector<int>(n + 1));
+    for (int i = 1; i <= n; i++) {
+        int t;
+        for (int j = 1; j <= n; j++) {
             cin >> t;
-            total += t;
-            v[i][j] = total;
+            v[i][j] = t + v[i - 1][j] + v[i][j - 1] - v[i - 1][j - 1];
         }
     }
     int x1, y1, x2, y2;
     for (int i = 0; i < m; i++) {
         int res = 0;
         cin >> x1 >> y1 >> x2 >> y2;
-        x1--; y1--; x2--; y2--;
-        for (int j = x1; j <= x2; j++) {
-            res += v[j][y2];
-            if (y1 != 0) res -= v[j][y1 - 1];
-        }
+        res = v[x2][y2] - v[x1 - 1][y2] - v[x2][y1 - 1] + v[x1 - 1][y1 - 1];
         cout << res << '\n';
     }
 }
