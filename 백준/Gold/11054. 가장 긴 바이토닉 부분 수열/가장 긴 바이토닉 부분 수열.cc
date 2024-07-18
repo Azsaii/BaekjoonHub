fe08205e4bacaf5002372154be2766bfee335154
KIM IN WOO <socczx@naver.com>
2024-07-18 23:32:50 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,45 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-int v[1000];
-vector<int> tmp;
-int find(int s, int e, int x) {
-    while (s < e) {
-        int m = (s + e) / 2;
-        if (tmp[m] < x) s = m + 1;
-        else e = m;
-    }
-    return e;
-}
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    vector<int> tmp;
+    int v[1000], r[1000], l[1000], n, maxv = 0;
     cin >> n;
     for (int i = 0; i < n; i++) cin >> v[i];
-    int r[1000] = {1, }, l[1000] = {1, }, cnt = 0, maxv = 0;
     tmp.push_back(v[0]); r[0] = 1;
     for (int i = 1; i < n; i++) {
-        if (tmp[cnt] < v[i]) { 
+        if (tmp.back() < v[i]) {
             tmp.push_back(v[i]); 
-            cnt++;
-            r[i] = cnt + 1;
+            r[i] = tmp.size();
         }
         else {
-            int p = find(0, tmp.size() - 1, v[i]);
-            tmp[p] = v[i];
-            r[i] = p + 1;
+            auto it = lower_bound(tmp.begin(), tmp.end(), v[i]);
+            *it = v[i];
+            r[i] = distance(tmp.begin(), it) + 1;
         }
     }
-    tmp.clear(); tmp.push_back(v[n - 1]); l[n - 1] = 1; cnt = 0;
+    tmp.clear(); tmp.push_back(v[n - 1]); l[n - 1] = 1;
     for (int i = n - 2; i >= 0; i--) {
-        if (tmp[cnt] < v[i]) {
+        if (tmp.back() < v[i]) {
             tmp.push_back(v[i]);
-            cnt++;
-            l[i] = cnt + 1;
+            l[i] = tmp.size();
         }
         else {
-            int p = find(0, tmp.size() - 1, v[i]);
-            tmp[p] = v[i];
-            l[i] = p + 1;
+            auto it = lower_bound(tmp.begin(), tmp.end(), v[i]);
+            *it = v[i];
+            l[i] = distance(tmp.begin(), it) + 1;
         }
     }
     for (int i = 0; i < n; i++)  maxv = max(maxv, r[i] + l[i]);
