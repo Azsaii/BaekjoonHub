542f90309274eddb108ee4ccf325a123af274486
KIM IN WOO <socczx@naver.com>
2024-07-19 21:59:22 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 12 ms, Memory: 8232 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -2,11 +2,56 @@
 #define ll long long
 #define pli pair<ll, int>
 using namespace std;
+// https://github.com/csehydrogen/algorithm/blob/master/fast_io.cc
+class FastIO {
+    int fd, bufsz;
+    char* buf, * cur, * end;
+public:
+    FastIO(int _fd = 0, int _bufsz = 1 << 20) : fd(_fd), bufsz(_bufsz) {
+        buf = cur = end = new char[bufsz];
+    }
+    ~FastIO() {
+        delete[] buf;
+    }
+    bool readbuf() {
+        cur = buf;
+        end = buf + bufsz;
+        while (true) {
+            size_t res = fread(cur, sizeof(char), end - cur, stdin);
+            if (res == 0) break;
+            cur += res;
+        }
+        end = cur;
+        cur = buf;
+        return buf != end;
+    }
+    int readint() {
+        while (true) {
+            if (cur == end) readbuf();
+            if (isdigit(*cur) || *cur == '-') break;
+            ++cur;
+        }
+        bool sign = true;
+        if (*cur == '-') {
+            sign = false;
+            ++cur;
+        }
+        int ret = 0;
+        while (true) {
+            if (cur == end && !readbuf()) break;
+            if (!isdigit(*cur)) break;
+            ret = ret * 10 + (*cur - '0');
+            ++cur;
+        }
+        return sign ? ret : -ret;
+    }
+};
+FastIO fio;
 int n;
 vector<vector<ll>> v;
 vector<ll> dst;
 void dij(int x) {
-    for (int i = 1; i <= n; i++) dst[i] = INT32_MAX;
+    dst.assign(n + 1, INT32_MAX);
     priority_queue<pli, vector<pli>, greater<pli>> pq;
     dst[x] = 0;
     pq.push({ 0, x });
@@ -25,15 +70,15 @@ void dij(int x) {
 }
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-    int e, a, b, c, v1, v2; cin >> n >> e;
+    int e, a, b, c, v1, v2; n = fio.readint(); e = fio.readint();
     v.assign(n + 1, vector<ll>(n + 1, INT32_MAX));
     dst.resize(n + 1);
     for (int i = 0; i < e; i++) {
-        cin >> a >> b >> c;
+        a = fio.readint(); b = fio.readint(); c = fio.readint();
         v[a][b] = c; v[b][a] = c;
         v[a][a] = 0; v[b][b] = 0;
     }
-    cin >> v1 >> v2;
+    v1 = fio.readint(); v2 = fio.readint();
     dij(v1);
     ll r1 = dst[1] + dst[v2];
     ll r2 = dst[n];
