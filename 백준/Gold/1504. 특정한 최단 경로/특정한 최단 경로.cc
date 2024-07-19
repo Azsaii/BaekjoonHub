48cc1ff00c4392153506f342f6552c84e1577249
KIM IN WOO <socczx@naver.com>
2024-07-19 21:55:17 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 48 ms, Memory: 7220 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,29 +1,46 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define pli pair<ll, int>
 using namespace std;
+int n;
+vector<vector<ll>> v;
+vector<ll> dst;
+void dij(int x) {
+    for (int i = 1; i <= n; i++) dst[i] = INT32_MAX;
+    priority_queue<pli, vector<pli>, greater<pli>> pq;
+    dst[x] = 0;
+    pq.push({ 0, x });
+    while (!pq.empty()) {
+        auto [cv, cn] = pq.top();
+        pq.pop();
+        if (cv > dst[cn]) continue;
+        for (int i = 1; i <= n; i++) {
+            ll nv = cv + v[cn][i];
+            if (dst[i] > nv) {
+                dst[i] = nv;
+                pq.push({ nv, i });
+            }
+        }
+    }
+}
 int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-    int n, e, a, b, c, v1, v2; cin >> n >> e;
-    vector<vector<ll>> v(n + 1, vector<ll>(n + 1, INT32_MAX));
+    int e, a, b, c, v1, v2; cin >> n >> e;
+    v.assign(n + 1, vector<ll>(n + 1, INT32_MAX));
+    dst.resize(n + 1);
     for (int i = 0; i < e; i++) {
         cin >> a >> b >> c;
         v[a][b] = c; v[b][a] = c;
         v[a][a] = 0; v[b][b] = 0;
     }
     cin >> v1 >> v2;
-    for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                ll nv = v[i][k] + v[k][j];
-                if (v[i][j] > nv) {
-                    v[i][j] = nv;
-                }
-            }
-        }
-    }
-    ll r1 = v[1][v1] + v[v1][v2] + v[v2][n];
-    ll r2 = v[1][v2] + v[v2][v1] + v[v1][n];
-    ll minv = min(r1, r2);
-    if (minv <= INT32_MAX) cout << minv;
-    else cout << -1;
+    dij(v1);
+    ll r1 = dst[1] + dst[v2];
+    ll r2 = dst[n];
+    if (r1 >= INT32_MAX && r2 >= INT32_MAX) { cout << -1; return 0; }
+    dij(v2);
+    r1 += dst[n];
+    r2 += dst[1] + dst[v1];
+    if (r1 >= INT32_MAX && r2 >= INT32_MAX) { cout << -1; return 0; }
+    cout << min(r1, r2);
 }
