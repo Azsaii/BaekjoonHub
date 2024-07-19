7e79006c543d1cc91eeb73060e430302204468ed
KIM IN WOO <socczx@naver.com>
2024-07-20 03:24:30 +0900

    [Gold IV] Title: 알파벳, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,24 +1,17 @@
 #include <bits/stdc++.h>
+#define MAX 21
 using namespace std;
 int n, m, maxv = 0;
-int v[21][21];
-bool vst[26] = { 0, };
-void dfs(pair<int, int> x, int sum) {
+int v[MAX][MAX], dp[MAX][MAX];
+void dfs(pair<int, int> x, int sum, int his) {
     auto [cr, cc] = x;
-    vst[v[cr][cc]] = true;
-    if (cr < n - 1 && !vst[v[cr + 1][cc]]) {
-        vst[v[cr + 1][cc]] = 1; dfs({ cr + 1, cc }, sum + 1); vst[v[cr + 1][cc]] = 0;
-    }
-    if (cr > 0 && !vst[v[cr - 1][cc]]) {
-        vst[v[cr - 1][cc]] = 1; dfs({ cr - 1, cc }, sum + 1); vst[v[cr - 1][cc]] = 0;
-    }
-    if (cc < m - 1 && !vst[v[cr][cc + 1]]) {
-        vst[v[cr][cc + 1]] = 1; dfs({ cr, cc + 1 }, sum + 1); vst[v[cr][cc + 1]] = 0;
-    }
-    if (cc > 0 && !vst[v[cr][cc - 1]]) {
-        vst[v[cr][cc - 1]] = 1; dfs({ cr, cc - 1 }, sum + 1); vst[v[cr][cc - 1]] = 0;
-    }
-    vst[v[cr][cc]] = false;
+    his |= (1 << v[cr][cc]);
+    if (dp[cr][cc] == his) return;
+    dp[cr][cc] = his;
+    if (cr < n - 1 && !(his & (1 << v[cr + 1][cc]))) dfs({ cr + 1, cc }, sum + 1, his);
+    if (cr > 0 && !(his & (1 << v[cr - 1][cc]))) dfs({ cr - 1, cc }, sum + 1, his);
+    if (cc < m - 1 && !(his & (1 << v[cr][cc + 1]))) dfs({ cr, cc + 1 }, sum + 1, his);
+    if (cc > 0 && !(his & (1 << v[cr][cc - 1]))) dfs({ cr, cc - 1 }, sum + 1, his);
     maxv = max(maxv, sum);
 }
 int main() {
@@ -26,6 +19,6 @@ int main() {
     cin >> n >> m;
     char c;
     for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) { cin >> c; v[i][j] = c - 'A'; }
-    dfs({ 0, 0 }, 1);
+    dfs({ 0, 0 }, 1, 0);
     cout << maxv;
 }
