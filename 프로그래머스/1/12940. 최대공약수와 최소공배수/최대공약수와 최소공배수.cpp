127e8c1ce5faac129fa472dc510bff0f33c30bc0
KIM IN WOO <socczx@naver.com>
2026-04-23 10:13:59 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.31 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> ret;
+    if(n > m) swap(n, m);
+    
+    for(int i = n; i >= 1; i--){
+        if(n % i == 0 && m % i == 0){
+            ret.push_back(i);
+            break;
+        }
+    }
+    
+    int maxv = n * m;
+    for(int i = m; i <= maxv; i++){
+        if(i % n == 0 && i % m == 0){
+            ret.push_back(i);
+            break;
+        }
+    }
+    // if(ret[0] == 1){
+    //     ret.push_back(maxv);
+    // }
+    // else {
+    //     for(int i = m; i <= maxv; i += m){
+    //         if(i % n == 0){
+    //             ret.push_back(i);
+    //             break;
+    //         }
+    //     }
+    // }
+    return ret;
+}
